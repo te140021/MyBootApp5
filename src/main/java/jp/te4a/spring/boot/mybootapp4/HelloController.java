3641aa1170bea51035df457a8f21656a0e258df4
140021 <140021@e0412-11.JC-21.local>
2019-06-28 12:01:55 +0900

    a

--- src/main/java/jp/te4a/spring/boot/mybootapp4/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/mybootapp4/HelloController.java
@@ -1,14 +1,26 @@
 package jp.te4a.spring.boot.mybootapp4;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "this is Spring Boot sample.";
+	@RequestMapping("/")
+    public String index(Model model) {
+        //return "this is Spring Boot sample.";
+        return "index";
+	}
+    @RequestMapping(value="/post", method=RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1") String text1) {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", "you write '" + text1 + "'!!!");
+        return mv;
     }
-}
 
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Index</title>
+</head>
+<body>
+    <h1>Index Page</h1>
+    <p>this is sample page for Spring Boot!</p>
+     <p>${msg}</p>
+     <form method="post" action="/post">
+        <input type="text"name="text1">
+        <input type="submit">
+    </form>
+     
+</body>
+</html>
